dc7241621d979c12a81d9060dceddec97fde5fd5
jonas de boer <jonasdeboer02@gmail.com>
2020-04-07 15:24:18 +0200

    Kan nu schrijven naar json, en lezen van json. Verwijderen werkt echter nog niet en de programma object heeft nog alleen tijd datum en programmaID

--- planner/Scheduler2/Schedulerr/Schedulerr/Planning.cs
+++ planner/Scheduler2/Schedulerr/Schedulerr/Planning.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.IO;
 using Newtonsoft.Json;
+
 namespace Schedulerr
 {
     public class Planning
@@ -9,25 +11,139 @@ namespace Schedulerr
         public string naam { get; set; }
         public Programma[] inhoud;
 
+        public Planning()
+        {
+            this.LeesJson();
+        }
+        public void PrintInhoud()
+        {
+            Console.Clear();
+            Console.WriteLine("Programmas:");
+            for (int i = 0; i < this.inhoud.Length; i++)
+            {
+                try
+                {
+                    Console.WriteLine($"\n{this.inhoud[i].programmaid}.   Datum: {this.inhoud[i].datum}\n     Tijd: {this.inhoud[i].tijd}");
+                }
+                catch
+                {
+                    Console.WriteLine("");
+                }
+            }
+        }
+        public int KiesId()
+        {
+            int max = 1;
+            for (int i = 0; i < this.inhoud.Length; i++)
+            {
+                if (this.inhoud[i] != null)
+                {
+                    if (this.inhoud[i].programmaid == max)
+                    {
+                        max++;
+                        i = 0;
+                    }
+                }
+             
+            }
+            return max;
+            
+            
+        }
         public void ProgrammaToevoegen(string datum,string tijd)
         {
+            
             Programma[] res = new Programma[this.inhoud.Length+1];
             int i = 0;
             for(i = 0;i < this.inhoud.Length; i++)
             {
                 res[i] = this.inhoud[i];
             }
-            res[i + 1] = new Programma() { 
-                programmaid = res[i].programmaid + 1,
-                datum = datum,
-                tijd = tijd
-            };
+            for (int j = 0; j < res.Length; j++)
+            {
+                if (res[j] == null)
+                {
+                    res[j] = new Programma()
+                    {
+                        programmaid = this.KiesId(),
+                        datum = datum,
+                        tijd = tijd
+                    };
+                }
+            }
+            
+            this.inhoud = res;
+        }
+        public int VindIndexDoorId(int id)
+        {
+            for(int i = 0; i < this.inhoud.Length; i++)
+            {
+                if(this.inhoud[i].programmaid == id)
+                {
+                    return i;
+                }
+            }
+            return -99;
+        }
+        public Programma VindObjectDoorId(int id)
+        {
+            for (int i = 0; i < this.inhoud.Length; i++)
+            {
+                if (this.inhoud[i].programmaid == id)
+                {
+                    return inhoud[i];
+                }
+            }
+            return null;
+        }
+        public void VerwijderProgramma(int n)
+        {
+            int index = VindIndexDoorId(n);
+            this.inhoud[index] = null;
+            this.VerwijderNulls();
+        }
+
+        public void VerwijderNulls()
+        {
+            int count = 0;
+            for(int i = 0; i < this.inhoud.Length;)
+            {
+                if (this.inhoud[i] == null)
+                {
+                    count++;
+                }
+            }
+            Programma[] res = new Programma[this.inhoud.Length - count];
+            int j = 0;
+            for(int i = 0; i < this.inhoud.Length; i++)
+            {
+                if(this.inhoud[i] != null)
+                {
+                    res[j] = this.inhoud[i];
+                    j++;
+                }
+            }
             this.inhoud = res;
         }
 
         public void UpdateNaarJson()
         {
-
+            using (StreamWriter file = File.CreateText(@"C:\Users\jonas\Documents\Cinema-ProjB\planner\Scheduler2\Schedulerr\Schedulerr\Planning.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                
+                serializer.Serialize(file, this.inhoud);
+                
+            }
         }
+        public void LeesJson()
+        {
+            using (StreamReader file = File.OpenText(@"C:\Users\jonas\Documents\Cinema-ProjB\planner\Scheduler2\Schedulerr\Schedulerr\Planning.json"))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                this.inhoud = (Programma[])serializer.Deserialize(file, typeof(Programma[]));
+            }
+        }
+        
     }
 }
--- planner/Scheduler2/Schedulerr/Schedulerr/Program.cs
+++ planner/Scheduler2/Schedulerr/Schedulerr/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 using Newtonsoft.Json;
 
 
@@ -11,24 +12,63 @@ namespace Schedulerr
     
     class Program
     {
-        public static void SerializeObject(Planning p)
-        {
-
-        }
+        
         static void Main(string[] args)
         {
-            
+            Planning planning = new Planning()
+            {
+                naam = "planning"
+            };
             bool exit = false;
             while(exit != true)
             {
-                Console.WriteLine("[A] zie planning\n[B] Maak programma aan\n[C] Verwijder programma [E] Verlaat");
+                Console.Clear();
+                Console.WriteLine("[A] zie planning\n[B] Maak programma aan\n[C] Verwijder programma\n[E] Verlaat");
                 string opdracht = Console.ReadLine();
                 switch (opdracht)
                 {
                     case "a":
                     case "A":
+                        planning.PrintInhoud();
+                        Console.WriteLine("Druk op spatie om door te gaan");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter)
+                        {
+                            Thread.Sleep(1);
+                        }
                         break;
+                    case "b":
+                    case "B":
+                        Console.Clear();
+                        Console.WriteLine("Schrijf datum in syntax: DD/MM/YYYY");
+                        string datum = Console.ReadLine();
+                        Console.Clear();
+                        Console.WriteLine("Schrijf starttijd in syntax: hh:mm");
+                        string tijd = Console.ReadLine();
+                        Console.Clear();
+                        planning.ProgrammaToevoegen(datum, tijd);
+                        Console.WriteLine("Programma toegevoegd, druk op spatie om door te gaan");
+                        while(Console.ReadKey().Key != ConsoleKey.Enter)
+                        {
+                            Thread.Sleep(1);
+                        }
+                        break;
+                    case "c":
+                    case "C":
+                        planning.PrintInhoud();
+                        Console.WriteLine("Welk programma wilt u verwijderen(typ het nummer)");
+                        string input = Console.ReadLine();
 
+                        Console.WriteLine("Programma verwijderd, druk op spatie om door te gaan");
+                        while (Console.ReadKey().Key != ConsoleKey.Enter)
+                        {
+                            Thread.Sleep(1);
+                        }
+                        break;
+                    case "e":
+                    case "E":
+                        planning.UpdateNaarJson();
+                        exit = true;
+                        break;
                 }
             }
         }
