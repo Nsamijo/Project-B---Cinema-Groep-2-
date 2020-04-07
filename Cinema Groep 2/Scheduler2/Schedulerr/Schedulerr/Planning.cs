8e8dbf58c422d316ace1c58a8c1ec39e02c3a5c1
jonas de boer <jonasdeboer02@gmail.com>
2020-04-07 10:26:47 +0200

    Opnieuw begonnen en een andere duidelijkere indeling gebruikt

---
+++ Cinema Groep 2/Scheduler2/Schedulerr/Schedulerr/Planning.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+namespace Schedulerr
+{
+    public class Planning
+    {
+        public string naam { get; set; }
+        public Programma[] inhoud;
+
+        public void ProgrammaToevoegen(string datum,string tijd)
+        {
+            Programma[] res = new Programma[this.inhoud.Length+1];
+            int i = 0;
+            for(i = 0;i < this.inhoud.Length; i++)
+            {
+                res[i] = this.inhoud[i];
+            }
+            res[i + 1] = new Programma() { 
+                programmaid = res[i].programmaid + 1,
+                datum = datum,
+                tijd = tijd
+            };
+            this.inhoud = res;
+        }
+
+        public void UpdateNaarJson()
+        {
+
+        }
+    }
+}
---
+++ Cinema Groep 2/Scheduler2/Schedulerr/Schedulerr/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+
+
+namespace Schedulerr
+{
+    
+
+    
+    
+    class Program
+    {
+        public static void SerializeObject(Planning p)
+        {
+
+        }
+        static void Main(string[] args)
+        {
+            
+            bool exit = false;
+            while(exit != true)
+            {
+                Console.WriteLine("[A] zie planning\n[B] Maak programma aan\n[C] Verwijder programma [E] Verlaat");
+                string opdracht = Console.ReadLine();
+                switch (opdracht)
+                {
+                    case "a":
+                    case "A":
+                        break;
+
+                }
+            }
+        }
+    }
+}
