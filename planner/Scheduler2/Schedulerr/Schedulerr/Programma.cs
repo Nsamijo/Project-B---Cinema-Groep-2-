29f8e0656619a8962da648c8c26c0b0d18203a34
jonas de boer <jonasdeboer02@gmail.com>
2020-04-07 10:32:56 +0200

    Branch verwijdert en nieuwe branch gemaakt "filmbeheer"

---
+++ planner/Scheduler2/Schedulerr/Schedulerr/Programma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Schedulerr
+{
+    public class Programma
+    {
+        public int programmaid { get; set; }
+        public string datum { get; set; }
+        public string tijd { get; set; }
+
+        
+    }
+}
