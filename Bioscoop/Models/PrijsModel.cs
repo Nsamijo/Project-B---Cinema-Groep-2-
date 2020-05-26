b80384edef7646b35447392b82fc1b5f2a943406
Janichi <jan.roozemond@gmail.com>
2020-05-26 21:58:49 +0200

    Main reservatie bestanden. Kan alle functionaliteiten van het reserveren voor de klant. Alle data vatbaar voor reserveren staat hierin verwerkt.

---
+++ Bioscoop/Models/PrijsModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bioscoop.Models
+{
+    class PrijsModel
+    {
+        public string Soort { get; set; }
+        public string Prijs { get; set; }
+
+        public PrijsModel() { }
+        public PrijsModel(string prijs, string soort )
+        {
+            Prijs = prijs;
+            Soort = soort;
+        }
+
+    }
+}
---
+++ Bioscoop/Models/ReserveringModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bioscoop.Models
+{
+    class ReserveringModel
+    {
+        public int ReserveringId { get; set; }
+        public string Code { get; set; }
+        public int ProgrammaId { get; set; }
+        public List<int> StoelId { get; set; }
+        public int Aantal { get; set; }
+        public string Totaal { get; set; }
+
+        public ReserveringModel() {
+            StoelId = new List<int>();
+        }
+
+        public ReserveringModel(int reserveringId, string code, int programmaId, List<int> stoelId, int aantal, string totaal)
+        {
+            ReserveringId = reserveringId;
+            Code = code;
+            ProgrammaId = programmaId;
+            StoelId = stoelId;
+            Aantal = aantal;
+            Totaal = totaal;
+        }
+    }
+}
